8839ac08ef2e6ded68e451b16550da38b2721af5
HaoboZ <haobozhang9081@gmail.com>
2019-10-03 14:49:14 -0700

    remove unneeded ref

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import MovementScene from './examples/movement/scene';
-import TictactoeScene from './examples/tictactoe/scene';
 import Connect from './library/connect';
 import Game from './library/game';
 import store from './library/redux/store';
