891044aa7cb7a7c31b1cbdbc53ed137dabb47424
HaoboZ <haobozhang9081@gmail.com>
2019-11-05 08:39:49 -0800

    using imports again, fixing bundle size

--- server/babel.config.js
+++ server/babel.config.js
@@ -15,7 +15,43 @@ module.exports = api => {
 					'legacy': true
 				}
 			],
-			'transform-class-properties'
+			'transform-class-properties',
+			[
+				'import',
+				{
+					'libraryName':             '@material-ui/core',
+					'libraryDirectory':        'esm',
+					'camel2DashComponentName': false
+				},
+				'core'
+			],
+			[
+				'import',
+				{
+					'libraryName':             '@material-ui/core/styles',
+					'libraryDirectory':        '../esm/styles',
+					'camel2DashComponentName': false
+				},
+				'core/styles'
+			],
+			[
+				'import',
+				{
+					'libraryName':             '@material-ui/styles',
+					'libraryDirectory':        'esm',
+					'camel2DashComponentName': false
+				},
+				'styles'
+			],
+			[
+				'import',
+				{
+					'libraryName':             '@material-ui/icons',
+					'libraryDirectory':        'esm',
+					'camel2DashComponentName': false
+				},
+				'icons'
+			]
 		]
 	};
 };
